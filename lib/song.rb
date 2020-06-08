decbeb22de8144714cf1ab50d95c2a823cee5288
Jack Quinoa <jquinoa208@gmail.com>
2020-06-08 17:18:03 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+require "pry"
+class Song 
+  
+  attr_accessor :name, :artist 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.new_by_filename(filename)
+    artist, title  = filename.split(" - ")
+   # binding.pry 
+    self.new(title).tap {|song| 
+      song.artist = Artist.find_or_create_by_name(artist)
+      song.artist.add_song(song)
+      }
+  end
+  
+  def artist_name= (name_of_artist)
+    # Artist.find_or_create_by_name(name_of_artist)
+    
+    
+    Artist.all.find {|artist| artist == name_of_artist} ? name_of_artist : self.artist = name_of_artist
+  end
+end
+
+
